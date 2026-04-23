ee578b3aad9df0b843491e50a6f49ab2426be049
Yash Lahase <yashlahase1234@gmail.com>
2026-04-23 19:20:00 +0530

    fix(backend): remove unnecessary escape in email regex

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -12,7 +12,7 @@ const userSchema = mongoose.Schema(
       required: [true, 'Please add an email'],
       unique: true,
       match: [
-        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
         'Please add a valid email',
       ],
     },
