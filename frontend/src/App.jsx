5dcfe0b17525198b69639fe8ac44894268b02671
Yash Lahase <yashlahase1234@gmail.com>
2026-04-27 13:13:21 +0530

    Register all newly created routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,14 @@ import ProductDetails from './pages/ProductDetails';
 import Cart from './pages/Cart';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import Checkout from './pages/Checkout';
+import Wishlist from './pages/Wishlist';
+import Category from './pages/Category';
+import OrderHistory from './pages/OrderHistory';
+import AdminProducts from './pages/AdminProducts';
+import AdminOrders from './pages/AdminOrders';
+import AdminProductEdit from './pages/AdminProductEdit';
+import Search from './pages/Search';
 
 const App = () => {
   return (
@@ -18,7 +26,17 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/category/:category" element={<div>Category Page</div>} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/category/:category" element={<Category />} />
+          <Route path="/orders" element={<OrderHistory />} />
+          <Route path="/search/:keyword" element={<Search />} />
+          
+          {/* Admin Routes */}
+          <Route path="/admin/products" element={<AdminProducts />} />
+          <Route path="/admin/orders" element={<AdminOrders />} />
+          <Route path="/admin/product/new" element={<AdminProductEdit />} />
+          <Route path="/admin/product/:id/edit" element={<AdminProductEdit />} />
         </Routes>
       </main>
       
